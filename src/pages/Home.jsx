bff5c87203a0271135f850d41901c99381f4df8c
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2025-03-04 23:47:34 -0300

    style/Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,17 +3,18 @@ import "@/styles/Home.css"
 
 export function Home() {
     return (
-        <main>
-            <span>So, you want to travel to</span>
-            <h1>Space</h1>
-            <p>
-                Let’s face it; if you want to go to space, you might as well genuinely go to
-                outer space and not hover kind of on the edge of it. Well sit back, and relax
-                because we’ll give you a truly out of this world experience!
-            </p>
-
-            <ExploreButton />
-        </main>
+        <body className="Mobile">
+            <main className="Home">
+                <span>So, you want to travel to</span>
+                <h1>Space</h1>
+                <p>
+                    Let’s face it; if you want to go to space, you might as well genuinely go to
+                    outer space and not hover kind of on the edge of it. Well sit back, and relax
+                    because we’ll give you a truly out of this world experience!
+                </p>
+                <ExploreButton />
+            </main>
+        </body>
     )
 }
 
