fde8c0c7f9f8d4a0739ee2d46fbf9b8f15a0b776
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2025-03-06 22:46:07 -0300

    style/Navbar

--- src/components/Navbar/MobileNav.jsx
+++ src/components/Navbar/MobileNav.jsx
@@ -1,13 +1,15 @@
 import logo from '@/assets/images/shared/logo.svg';
 import menuIcon from '@/assets/images/shared/icon-hamburger.svg';
 
+import styles from "@/styles/MobileNav.module.css";
+
 export function MobileNav() {
   return (
-    <nav>
-      <div>
+    <nav className={styles.NavbarMobile}>
+      <div className={styles.NavbarMobilelogo}>
         <img src={logo} alt="logo" />
       </div>
-      <div>
+      <div className={styles.NavbarMobileMenu}>
         <img src={menuIcon} alt="menu" />
       </div>
     </nav>
