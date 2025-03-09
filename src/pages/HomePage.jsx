6392ba88edce97301de46f2dbc90f9fb8d4b4d55
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2025-03-08 23:54:44 -0300

    refactor/NavBar.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,14 @@
 import { HomeText } from "@/components/HomeText/HomeText";
 import { ExploreButton } from "@/components/ExploreButton/ExploreButton";
-import { MobileNav } from "@/components/Navbar/MobileNav";
+import { Navbar } from "@/components/Navbar/NavBar";
+// import { MobileNav } from "@/components/Navbar/MobileNav";
 
 import styles from "@/styles/HomePage.module.css";
 
 export function HomePage() {
     return (
         <main className={styles.HomePage}>
-            <MobileNav/>
+            <Navbar/>
             <HomeText />
             <ExploreButton />
         </main>
