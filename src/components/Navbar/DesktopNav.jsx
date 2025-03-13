4bab8488b390946b9849a70184be4101f6fd6948
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2025-03-12 22:15:04 -0300

    feat/ Desktop  NavBar.jsx

--- src/components/Navbar/DesktopNav.jsx
+++ src/components/Navbar/DesktopNav.jsx
@@ -1,4 +1,29 @@
+import logo from '@/assets/images/shared/logo.svg';
+
 export default function DesktopNav() {
-    return null; // Retorna nada por enquanto
-  }
-  
+  return (
+    <nav className={styles.NavbarDesktop}>
+      <div>
+        <img src={logo} alt="logo" />
+      </div>
+      <div>
+        <div>
+          <span>00</span>
+          <p>Home</p>
+        </div>
+        <div>
+          <span>01</span>
+          <p>Destination</p>
+        </div>
+        <div>
+          <span>02</span>
+          <p>Crew</p>
+        </div>
+        <div>
+          <span>03</span>
+          <p>Technology</p>
+        </div>
+      </div>
+    </nav>
+  );
+}
