886bcb27599c15301d315f6a3aa2ad5e0c351822
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2025-03-13 23:07:05 -0300

    feat/ Desktop  NavBar.jsx

--- src/components/Navbar/DesktopNav.jsx
+++ src/components/Navbar/DesktopNav.jsx
@@ -1,29 +1,62 @@
 import logo from '@/assets/images/shared/logo.svg';
+import styles from "./DesktopNav.module.css";
 
 export default function DesktopNav() {
   return (
     <nav className={styles.NavbarDesktop}>
-      <div>
+
+      <div className={styles.NavbarDesktoplogo}>
         <img src={logo} alt="logo" />
       </div>
-      <div>
-        <div>
-          <span>00</span>
-          <p>Home</p>
+
+      {/* <div className={styles.BarDesktop}>
+        <div className={styles.BarDesktop_box}>
+          <span className={styles.BarDesktop_number}>
+            00
+          </span>
+          <p className={styles.BarDesktop_text}>
+            Home
+          </p>
         </div>
-        <div>
-          <span>01</span>
-          <p>Destination</p>
+        <div className={styles.BarDesktop_box}>
+          <span className={styles.BarDesktop_number}>
+            01
+            </span>
+          <p className={styles.BarDesktop_text}>
+            Destination
+            </p>
         </div>
-        <div>
-          <span>02</span>
-          <p>Crew</p>
+        <div className={styles.BarDesktop_box}>
+          <span className={styles.BarDesktop_number}>
+            02
+            </span>
+          <p className={styles.BarDesktop_text}>
+            Crew
+            </p>
         </div>
-        <div>
-          <span>03</span>
-          <p>Technology</p>
+        <div className={styles.BarDesktop_box}>
+          <span className={styles.BarDesktop_number}>
+            03
+            </span>
+          <p className={styles.BarDesktop_text}>
+            Technology
+            </p>
         </div>
-      </div>
+      </div> */}
+
+<div className={styles.BarDesktop}>
+  {["Home", "Destination", "Crew", "Technology"].map((item, index) => (
+    <div key={index} className={styles.BarDesktop_box}>
+      <span className={styles.BarDesktop_number}>
+        {`0${index}`}
+      </span>
+      <p className={styles.BarDesktop_text}>
+        {item}
+      </p>
+    </div>
+  ))}
+</div>
+
     </nav>
   );
 }
