7d018c32bbb19e0e2e0069e518df25d76d216170
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2025-03-15 23:11:01 -0300

    feat/ Desktop  NavBar.jsx

--- src/components/Navbar/DesktopNav.jsx
+++ src/components/Navbar/DesktopNav.jsx
@@ -9,53 +9,18 @@ export default function DesktopNav() {
         <img src={logo} alt="logo" />
       </div>
 
-      {/* <div className={styles.BarDesktop}>
-        <div className={styles.BarDesktop_box}>
-          <span className={styles.BarDesktop_number}>
-            00
-          </span>
-          <p className={styles.BarDesktop_text}>
-            Home
-          </p>
-        </div>
-        <div className={styles.BarDesktop_box}>
-          <span className={styles.BarDesktop_number}>
-            01
+      <div className={styles.BarDesktop}>
+        {["Home", "Destination", "Crew", "Technology"].map((item, index) => (
+          <div key={index} className={styles.BarDesktop_box}>
+            <span className={styles.BarDesktop_number}>
+              {`0${index}`}
             </span>
-          <p className={styles.BarDesktop_text}>
-            Destination
+            <p className={styles.BarDesktop_text}>
+              {item}
             </p>
-        </div>
-        <div className={styles.BarDesktop_box}>
-          <span className={styles.BarDesktop_number}>
-            02
-            </span>
-          <p className={styles.BarDesktop_text}>
-            Crew
-            </p>
-        </div>
-        <div className={styles.BarDesktop_box}>
-          <span className={styles.BarDesktop_number}>
-            03
-            </span>
-          <p className={styles.BarDesktop_text}>
-            Technology
-            </p>
-        </div>
-      </div> */}
-
-<div className={styles.BarDesktop}>
-  {["Home", "Destination", "Crew", "Technology"].map((item, index) => (
-    <div key={index} className={styles.BarDesktop_box}>
-      <span className={styles.BarDesktop_number}>
-        {`0${index}`}
-      </span>
-      <p className={styles.BarDesktop_text}>
-        {item}
-      </p>
-    </div>
-  ))}
-</div>
+          </div>
+        ))}
+      </div>
 
     </nav>
   );
