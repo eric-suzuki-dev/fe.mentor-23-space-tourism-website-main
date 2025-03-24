444db3d6285c77c7d0d023cca38e9c68e99292c0
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2025-03-23 23:39:20 -0300

    styles/ Desktop  NavBar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,6 @@
 import { HomeText } from "@/components/HomeText/HomeText";
 import { ExploreButton } from "@/components/ExploreButton/ExploreButton";
 import  NavBar from "@/components/NavBar/NavBar";
-// import { MobileNav } from "@/components/Navbar/MobileNav";
 
 import styles from "@/styles/HomePage.module.css";
 
