543efb39c551849352d63e22c1f26f11d34e8fab
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2025-03-26 22:57:07 -0300

    modification/HomePage

--- src/components/Navbar/MobileNav.jsx
+++ src/components/Navbar/MobileNav.jsx
@@ -1,17 +1,41 @@
-import logo from '@/assets/images/shared/logo.svg';
-import menuIcon from '@/assets/images/shared/icon-hamburger.svg';
+// import logo from '@/assets/images/shared/logo.svg';
+// import menuIcon from '@/assets/images/shared/icon-hamburger.svg';
 
-import styles from "./MobileNav.module.css";
+// import styles from "./MobileNav.module.css";
 
-export function MobileNav() {
+// export function MobileNav() {
+//   return (
+//     <nav className={styles.NavbarMobile}>
+//       <div className={styles.NavbarMobilelogo}>
+//         <img src={logo} alt="logo" />
+//       </div>
+//       <div className={styles.NavbarMobileMenu}>
+//         <img src={menuIcon} alt="menu" />
+//       </div>
+//     </nav>
+//   );
+// }
+
+// import React from "react";
+import { Logo } from "./Logo";
+import "./style.css";
+
+export const Navigation = () => {
   return (
-    <nav className={styles.NavbarMobile}>
-      <div className={styles.NavbarMobilelogo}>
-        <img src={logo} alt="logo" />
+    <div className="navigation">
+      <div className="logo-wrapper">
+        <Logo className="logo-instance" />
       </div>
-      <div className={styles.NavbarMobileMenu}>
-        <img src={menuIcon} alt="menu" />
+
+      <div className="menu">
+        <div className="div">
+          <div className="shape" />
+
+          <div className="shape-2" />
+
+          <div className="shape-3" />
+        </div>
       </div>
-    </nav>
+    </div>
   );
-}
+};
