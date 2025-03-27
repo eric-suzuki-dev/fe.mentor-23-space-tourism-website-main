420cf90e53cce0b2cabc53a778c94dcc540352c3
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2025-03-26 23:02:31 -0300

    modification/HomePage

--- src/components/ExploreButton/ExploreButton.jsx
+++ src/components/ExploreButton/ExploreButton.jsx
@@ -1,10 +1,20 @@
-// import React from "react";
-import styles from "./ExploreButton.module.css";
+// import styles from "./ExploreButton.module.css";
 
-export function ExploreButton() {
-    return (
-        <button type="button" className={styles.ExploreButton}>
-            <span>Explore</span>
-        </button>
-    );
-}
+// export function ExploreButton() {
+//     return (
+//         <button type="button" className={styles.ExploreButton}>
+//             <span>Explore</span>
+//         </button>
+//     );
+// }
+
+// import React from "react";
+import "./style.css";
+
+export const DisplayButton = () => {
+  return (
+    <div className="display-button">
+      <div className="text-wrapper">EXPLORE</div>
+    </div>
+  );
+};
